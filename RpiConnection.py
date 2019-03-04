8fec41907d523289270ac9825e9a4f5b3c8cebc5
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-03-04 12:19:58 +0800

    Add RpiConnection

---
+++ RpiConnection.py
@@ -0,0 +1,19 @@
+from arrow_finder import ArrowFinder
+
+def to_byte(i, length=1, byteorder="little"):
+	return i.to_bytes(length,byteorder)
+
+DEST_HEADER_TO_PC = to_byte(8)
+
+class RpiConnection:
+	def __init__(self):
+		self.arrowFinder = ArrowFinder()
+		self.ready = False
+	def send(self, bytes):
+		self.ready = True
+
+	def recv(self):
+		while not self.ready:
+			pass
+		result = to_byte(bool(self.arrowFinder.getArrows()))
+		return DEST_HEADER_TO_PC + result
