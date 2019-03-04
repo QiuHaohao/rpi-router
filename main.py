345dccef89b0575269b2bd8ec148adf012827226
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-03-04 12:24:22 +0800

    Add rpi connection to main thread

--- main.py
+++ main.py
@@ -4,14 +4,17 @@ import threading
 from tcp import TCPServer
 from BTServer import BTServer
 from SerialArduino import SerialArduino
+from RpiConnection import RpiConnection
+
 from ReceiveThread import ReceiveThread
 from SendThread import SendThread
 
 queue_lock = threading.Lock()
 data_queue = queue.Queue()
 
-bt = BTServer()
 ser = SerialArduino()
+rpi = RpiConnection()
+bt = BTServer()
 pc = TCPServer()
 
 bt.init_connection()
@@ -26,7 +29,7 @@ print("All Connections Up! Waiting for message...")
 # receivers = [ReceiverStub(id=i) for i in range(3)]
 # senders = [SenderStub(id=i) for i in range(3)]
 
-connections = [ser,None,bt,pc]
+connections = [ser,rpi,bt,pc]
 
 receive_threads = [
 	ReceiveThread(threadID=i, 
@@ -35,13 +38,13 @@ receive_threads = [
 					lock=queue_lock, 
 					queue=data_queue
 				) 
-	for i in range(3)
+	for i in range(len(connections))
 ]
 
 for t in receive_threads:
 	t.start()
 
-send_thread = SendThread(threadID=3, 
+send_thread = SendThread(threadID=len(connections), 
 							name="Send_Thread", 
 							scheme=connections, 
 							lock=queue_lock, 
