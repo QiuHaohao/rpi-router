f1de2e1e8d64d44011f1fdea1822ae83860861e7
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-03-05 14:15:25 +0800

    Fix SerialArduino

--- connections/SerialArduino.py
+++ connections/SerialArduino.py
@@ -2,22 +2,26 @@ import serial
 
 class SerialArduino(object):
     def __init__(self,rate=9600):
-        self.baudrate = rate
+        self.baud_rate = rate
         self.port = '/dev/ttyACM0' #can be either 0 or 1 depending on which port connected to Arduino
-   
+        self.ser = None
+
     def send(self, msg):
         try :
-            self.write(msg) #msg is in bytes form
+            self.ser.write(msg) #msg is in bytes form
         except Exception as e :
             print("\nError Message : %s",str(e))
-
 
     def recv(self):
         try:
             #return ser.read(1)
-            return self.readline() #this requires Arduino side to send '\n' as EOL
+            return self.ser.readline() #this requires Arduino side to send '\n' as EOL
         except Exception as e:
             print("\nError Message : %s",str(e))
 
     def init_connection(self):
-        pass
+        try:
+            self.ser = serial.Serial(self.port, self.baud_rate)
+            print("Serial connected.")
+        except Exception as e:
+            print("Serial not connected: {}".format(e))
