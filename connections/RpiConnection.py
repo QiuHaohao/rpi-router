ca29f42cb7496426c912a62f863708cc7038689f
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-03-06 15:50:47 +0800

    Refactor RpiConnection

--- connections/RpiConnection.py
+++ connections/RpiConnection.py
@@ -14,28 +14,37 @@ CODE_CAPTURING_FINISHED = to_byte(21)
 CODE_ARROW_DETECTED = to_byte(22)
 
 class RpiConnection:
-	class DetectThread(threading.Thread)：
-		def __init__(self, in_queue, detect, on_detected):
-			self.in_queue = in_queue
-			self.detect = detect
+	class DetectionThread(threading.Thread)：
+		def __init__(self, got_msg, get_first_msg, on_finish_capturing, on_detected):
+			self.arrowFinder = ArrowFinder()
+			self.got_msg = got_msg
+			self.get_first_msg = get_first_msg
+			self.on_finish_capturing = on_finish_capturing
 			self.on_detected = on_detected
 		def run(self):
 			while True:
-				if not self.in_queue.empty():
-					msg = self.in_queue.get()
+				if not self.got_msg:
+					msg = self.get_first_msg
 					msg_payload = msg[1:]
 					got_arrow = self.detect()
 					if got_arrow:
 						self.on_detected(msg_payload)
 
+		def detect(self):
+			return bool(
+				self.arrowFinder.getArrows(
+					after_capture = self.on_finish_capturing
+				)
+			)
+
 	def __init__(self):
-		self.arrowFinder = ArrowFinder()
 		self.ready = False
 		self.in_queue = queue.Queue()
 		self.out_queue = queue.Queue()
-		self.detech_thread = DetectThread(
-			in_queue=self.in_queue,
-			detect=self.detect,
+		self.detech_thread = DetectionThread(
+			got_msg=lambda: not self.in_queue.empty(),
+			get_first_msg=self.in_queue.get,
+			on_finish_capturing=self.put_capturing_finished,
 			on_detected=self.put_arrow_detected
 		)
 		self.detech_thread.start()
@@ -48,13 +57,6 @@ class RpiConnection:
 		while self.out_queue.empty():
 			pass
 		return self.out_queue.get()
-
-	def detect(self):
-		return bool(
-			self.arrowFinder.getArrows(
-				after_capture = self.put_capturing_finished
-			)
-		)
 
 	def put_capturing_finished(self):
 		self.out_queue.put(
