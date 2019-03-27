42d0c47b34652f88e3b4de9ec3193378a787cd9c
QiuHaoHao <qiuh0008@e.ntu.edu.sg>
2019-03-27 13:44:08 +0800

    Remove wait_until_ser_avail

--- connections/SerialArduino.py
+++ connections/SerialArduino.py
@@ -1,21 +1,19 @@
 import serial
 
 class SerialArduino(object):
-    def __init__(self,rate=9600):
+    def __init__(self,rate=115200):
         self.baud_rate = rate
-        self.port = '/dev/ttyACM0' #can be either 0 or 1 depending on which port connected to Arduino
+        self.port = '/dev/ttyACM1' #can be either 0 or 1 depending on which port connected to Arduino
         self.ser = None
 
     def send(self, msg):
         try :
-            self.wait_until_ser_avail()
             self.ser.write(msg) #msg is in bytes form
         except Exception as e :
             print("\nError Message : %s",str(e))
 
     def recv(self):
         try:
-            self.wait_until_ser_avail()
             return self.ser.readline() #this requires Arduino side to send '\n' as EOL
         except Exception as e:
             print("\nError Message : %s",str(e))
@@ -26,7 +24,3 @@ class SerialArduino(object):
             print("Serial connected.")
         except Exception as e:
             print("Serial not connected: {}".format(e))
-
-    def wait_until_ser_avail(self):
-        while not self.ser.available():
-                pass
